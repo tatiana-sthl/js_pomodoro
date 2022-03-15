66d9d3ab77c587ef6c7a140aa7a3755333fca696
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2022-03-15 10:56:28 +0100

    update main.js styles.css and index.html

---
+++ main.js
@@ -0,0 +1,46 @@
+const timer = {
+    pomodoro: 25,
+    shortBreak: 5,
+    longBreak: 15,
+    longBreakInterval: 4,
+}
+
+const modeButtons = document.querySelector("#js-mode-buttons");
+modeButtons.addEventListener("click", handleMode);
+
+function updateClock() {
+    const { remainingTime } = timer;
+    const minutes = `${remainingTime.minutes}`.padStart(2, '0');
+    const seconds = `${remainingTime.seconds}`.padStart(2, '0');
+
+    const min = document.getElementById('js-minutes');
+    const sec = document.getElementById('je-seconds');
+    min.textContent = minutes;
+    sec.textContent = seconds;
+}
+
+function switchMode(mode) {
+    timer.mode = mode;
+    timer.remainingTime = {
+        total: timer[mode] * 60,
+        minutes: timer[mode],
+        seconds: 0,
+    };
+
+    document
+        .querySelectorAll("button[data-mode]")
+        .forEach(e => e.classList.remove("active"));
+    document.querySelector(`[data-mode="${mode}"]`).classList.add("active");
+    document.body.style.backgroundImage = `var(--${mode})`;
+
+    updateClock();
+}
+
+function handleMode(event) {
+    const { mode } = event.target.dataset;
+
+    if (!mode) return;
+
+    switchMode(mode);
+}
+
